e07ae12ad3f069303eaced54ee9e79f8948db80c
Claudio Scheer <claudioscheer@protonmail.com>
2020-05-15 00:12:00 -0300

    Additive heuristic following slide pseudocode

--- src/debug.py
+++ src/debug.py
@@ -12,43 +12,36 @@ from pddl.action import Action
 # pylint: enable=import-error
 
 
-class MaxHeuristic(Heuristic):
+class AdditiveHeuristic(Heuristic):
     def h(self, actions, state, goals):
-        """
-            I think that only positive preconditions/goals will not work well.
-            I will need to merge positive and negative states.
-            
-            goals: positive goals and negative goals in a tuple.
-        """
-        fact_level = [state]
-        action_level = []
-        # I think that only positive preconditions will not work.
-        positive_goals = goals[0]
-        max_cost = 0
+        reachable = [state]
+        goals_missing = goals[0]
+        goals_reached = None
+        last_state = [frozenset()]
+        add = 0
+        level = 0
+        while last_state[level] != reachable[level]:
+            goals_reached = goals_missing.intersection(reachable[level])
+            if len(goals_reached) > 0:
+                add += level * len(goals_reached)
+                goals_missing = goals_missing.difference(goals_reached)
+            if len(goals_missing) == 0:
+                return add
 
-        # While the goals are not in the state, keep seeking.
-        while not positive_goals.issubset(fact_level[max_cost]):
-            # Get all actions applicable to the current state level.
-            action_level.append(
-                [a for a in actions if a.positive_preconditions.issubset(fact_level[max_cost])]
-            )
-
-            # The next state will contain all the actions from previous states, plus the effects actions when executing the actions applicable to the current state.
-            fact_level.append(
-                fact_level[max_cost].union(
-                    [pre for a in action_level[max_cost] for pre in a.add_effects]
+            last_state.append(
+                frozenset(
+                    [a for a in actions if a.positive_preconditions.issubset(reachable[level])]
                 )
             )
+            reachable.append(
+                reachable[level].union([pre for a in last_state[level] for pre in a.add_effects])
+            )
+            level += 1
+        return float("inf")
 
-            # When the next state is the same as the current state, it means that there are no more effect actions that can reach the goal.
-            if fact_level[max_cost + 1] == fact_level[max_cost]:
-                return float("inf")
-            max_cost += 1
-        return max_cost
 
-
-dwr = os.path.join(dir_path, "../examples/dwr/dwr.pddl")
-pb1_dwr = os.path.join(dir_path, "../examples/dwr/pb1.pddl")
+tsp = os.path.join(dir_path, "../examples/tsp/tsp.pddl")
+pb1_tsp = os.path.join(dir_path, "../examples/tsp/pb1.pddl")
 
 
 def parse_domain_problem(domain, problem):
@@ -72,5 +65,5 @@ def test_heuristic(domain, problem, h, expected):
     )
 
 
-h = MaxHeuristic()
-test_heuristic(dwr, pb1_dwr, h, 6)
+h = AdditiveHeuristic()
+test_heuristic(tsp, pb1_tsp, h, 8)
